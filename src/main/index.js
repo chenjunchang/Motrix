98a339932a0e2d2e37cc0098fff206106852c555
chenjunchang <chenjack537@gmail.com>
2025-06-08 16:13:50 +0800

    update

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,14 @@
 import { app } from 'electron'
 import is from 'electron-is'
 import { initialize } from '@electron/remote/main'
+import fetch from 'node-fetch'
 
 import Launcher from './Launcher'
+
+// 添加fetch polyfill到全局对象
+if (!global.fetch) {
+  global.fetch = fetch
+}
 
 /**
  * initialize the main-process side of the remote module
